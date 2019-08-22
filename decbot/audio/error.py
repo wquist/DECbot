5c03efcc6112b0ad028eee433afc360a319ebc9b
Wyatt Lindquist <git.wquist@gmail.com>
2019-08-22 02:08:01 +0100

    update error classes

--- decbot/audio/error.py
+++ decbot/audio/error.py
@@ -1,5 +1,22 @@
+class MixerError(Exception):
+	""" An audio mixer IO error.
+
+	An error that occurs while loading audio data from disk.
+	"""
+	pass
+
 class RequestError(Exception):
+	""" A TTS request IO error.
+
+	An error that occurs when writing a request to disk.
+	"""
 	pass
 
 class TTSError(Exception):
+	""" A TTS invocation error.
+
+	An error that occurs when the external TTS command fails. This is raised
+	when the DEC invocation returns an error code other than zero. Information
+	written to stderr by DEC is usually included with this exception.
+	"""
 	pass
