e2e700dab0828fb0c00079d5e0f568cf358e137c
Wyatt Lindquist <git.wquist@gmail.com>
2019-08-22 03:02:08 +0100

    fix module inits

---
+++ decbot/audio/__init__.py
@@ -0,0 +1,4 @@
+from error import MixerError, RequestError, TTSError
+from mixer import Mixer
+from request import Request
+import tts
--- decbot/config/__init__.py
+++ decbot/config/__init__.py
@@ -1,3 +1,3 @@
-import error
+from error import ConfigError
 # Import the loader methods so they can be called directly from `config`.
 from loader import set_path, get
