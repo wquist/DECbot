98fb27828f42c700d83d40b32e8c1781725b65cd
Wyatt Lindquist <git.wquist@gmail.com>
2019-08-22 02:57:01 +0100

    add a stub for the package main

--- decbot/__main__.py
+++ decbot/__main__.py
@@ -1,1 +1,16 @@
-#
+from argparse import ArgumentParser
+
+import config
+
+parser = ArgumentParser(description = 'Run a Discord bot that can DECTalk.')
+parser.add_argument('-c', '--config', type = str,
+	help = 'use the specified path to load the configuration YAML')
+parser.add_argument('-V', '--verbose', action = 'store_true',
+	help = 'write detailed output to standard out')
+
+args = parser.parse_args()
+if args.config is not None:
+	config.set_path(args.config)
+
+# Run the bot...
+pass
