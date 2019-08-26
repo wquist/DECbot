c6f0feaef88a1e78c75e039ff81487a19b35f001
Wyatt Lindquist <git.wquist@gmail.com>
2019-08-26 03:01:12 +0100

    handle unknown command errors

--- decbot/bot/util.py
+++ decbot/bot/util.py
@@ -1,5 +1,5 @@
 from discord import DiscordException
-from discord.ext.commands import Cog, command, is_owner
+from discord.ext.commands import Cog, command, CommandNotFound, is_owner
 
 class Util(Cog):
 	""" A bot mixin handling miscellaneous functionality.
@@ -21,6 +21,21 @@ class Util(Cog):
 		This pulls messaging utilities from the text cog.
 		"""
 		self.send_message = self.bot.get_cog('Text').send_message
+
+	@Cog.listener()
+	async def on_command_error(self, ctx, err):
+		""" Handle unknown command errors.
+
+		This method is called when any error occurs, including those in valid
+		commands. Filter out only the unknown command exceptions to handle.
+
+		:param ctx: The context from the active error.
+		:type  ctx: discord.ext.commands.Context
+		:param err: The raised exception object.
+		:type  err: Exception
+		"""
+		if type(err) is CommandNotFound:
+			await self.send_message(ctx, 'I don\'t know how to do that.')
 
 	@command()
 	async def ping(self, ctx):
