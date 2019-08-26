5c7be6a8df96129dff44cca128853e4b6d048f35
Wyatt Lindquist <git.wquist@gmail.com>
2019-08-26 02:39:56 +0100

    document bot.create()

--- decbot/bot/__init__.py
+++ decbot/bot/__init__.py
@@ -5,10 +5,23 @@ from .util import Util
 from .voice import Voice
 
 def create(prefix = '!dec '):
-	client = Bot(command_prefix = prefix)
+	""" Create a new DECbot instance.
 
-	client.add_cog(Text(client))
-	client.add_cog(Util(client))
-	client.add_cog(Voice(client))
+	This method manages creating the `discord.py` client and attaching the cogs
+	defined in this module. Note that the resultant client is not logged in or
+	running; this must be done after the fact.
+
+	:param prefix: The prefix needed to invoke the bot within Discord. Note the
+	               space; the default uses it to act like a command group.
+	:type  prefix: str
+
+	:returns: The newly created bot instance.
+	:rtype:   discord.ext.commands.Bot
+
+	:raises discord.DiscordException: Errors from `discord.py` are propogated.
+	"""
+	client = Bot(command_prefix = prefix)
+	for Cog in [Text, Util, Voice]:
+		client.add_cog(Cog(client))
 
 	return client
