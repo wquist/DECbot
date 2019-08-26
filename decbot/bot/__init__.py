9ad2c146368190b7c56c63f71ae3a7e5f1849672
Wyatt Lindquist <git.wquist@gmail.com>
2019-08-26 01:09:17 +0100

    add extra utility commands

--- decbot/bot/__init__.py
+++ decbot/bot/__init__.py
@@ -1,12 +1,14 @@
 from discord.ext.commands import Bot
 
 from .text import TextCog
+from .util import UtilCog
 from .voice import VoiceCog
 
 def create(prefix = '!dec '):
 	client = Bot(command_prefix = prefix)
 
 	client.add_cog(TextCog(client))
+	client.add_cog(UtilCog(client))
 	client.add_cog(VoiceCog(client))
 
 	return client
---
+++ decbot/bot/util.py
@@ -0,0 +1,35 @@
+from discord import DiscordException
+from discord.ext.commands import Cog, command, is_owner
+
+class UtilCog(Cog):
+	""" A bot mixin handling miscellaneous functionality.
+
+	This sets up info commands and control commands targeting the bot owner.
+	"""
+	def __init__(self, bot):
+		""" Create a new utility mixin.
+
+		:param bot: The bot this cog will be added to.
+		:type  bot: discord.ext.commands.Bot
+		"""
+		self.bot = bot
+
+	@Cog.listener()
+	async def on_ready(self):
+		""" Retrieve sibling cogs and perform setup before the bot runs.
+
+		This pulls messaging utilities from the text cog.
+		"""
+		self.send_message = self.bot.get_cog('TextCog').send_message
+
+	@command()
+	async def ping(self, ctx):
+		""" check if DECbot is responsive and view server latency """
+		ms = round(self.bot.latency * 1000)
+		await self.send_message(ctx, 'Pong! ({} ms)'.format(ms))
+
+	@command()
+	@is_owner()
+	async def quit(self, ctx):
+		""" shutdown DECbot (owner only) """
+		await self.bot.logout()
