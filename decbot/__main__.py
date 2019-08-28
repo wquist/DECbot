e7b3922aef36f7c4bcef563391640ebef7c5d257
Wyatt Lindquist <git.wquist@gmail.com>
2019-08-28 02:49:16 +0100

    move startup into a method

--- decbot/__main__.py
+++ decbot/__main__.py
@@ -5,39 +5,45 @@ import sys
 from . import bot
 from . import config
 
-parser = ArgumentParser(description = 'Run a Discord bot that can DECTalk.')
-parser.add_argument('-c', '--config', type = str,
-	help = 'use the specified path to load the configuration YAML')
-parser.add_argument('-i', '--invite', type = int,
-	help = 'generate an oauth invite link for the given client ID')
-parser.add_argument('-p', '--permissions', type = int, default = 3149056,
-	help = 'use the given permissions mask (defaults to 3149056)')
+def main():
+	parser = ArgumentParser(description = 'Run a Discord bot that can DECTalk.')
+	parser.add_argument('-c', '--config', type = str,
+		help = 'use the specified path to load the configuration YAML')
+	parser.add_argument('-i', '--invite', type = int,
+		help = 'generate an oauth invite link for the given client ID')
+	parser.add_argument('-p', '--permissions', type = int, default = 3149056,
+		help = 'use the given permissions mask (defaults to 3149056)')
 
-args = parser.parse_args()
-if args.config:
-	config.set_path(args.config)
+	args = parser.parse_args()
+	if args.config:
+		config.set_path(args.config)
 
-if args.invite:
-	# Note that the default permissions are minimal; `DECbot` must be able to
-	# read and write text messages, and also view, join, and speak in voice
-	# ones. The default permissions also give DECbot the priority voice option,
-	# although this can be unchecked by the admin viewing the link.
-	link = '{}?client_id={}&scope=bot&permissions={}'.format(
-		'https://discordapp.com/oauth2/authorize',
-		args.invite,
-		args.permissions
-	)
+	if args.invite:
+		# Note that the default permissions are minimal; `DECbot` must be able
+		# to read and write text messages, and also view, join, and speak in
+		# voice ones. The default permissions also give DECbot the priority
+		# voice option, although this can be unchecked by the admin.
+		link = '{}?client_id={}&scope=bot&permissions={}'.format(
+			'https://discordapp.com/oauth2/authorize',
+			args.invite,
+			args.permissions
+		)
 
-	print('Tell your server admin to use this link:\n-', link)
-	sys.exit(0)
+		print(link)
+		return 0
 
-try:
-	client = bot.create()
-	token  = config.get('token')
+	try:
+		client = bot.create()
+		token  = config.get('token')
 
-	# Manually run the event loop, since `client.run()` does not seem the same.
-	# Exiting with a `^C` does not quit cleanly like this manual version.
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(client.start(token))
-except KeyboardInterrupt:
-	loop.run_until_complete(client.close())
+		# Manually run the event loop, since `client.run()` does not seem the
+		# same. Exiting with a `^C` does not quit cleanly like this version.
+		loop = asyncio.get_event_loop()
+		loop.run_until_complete(client.start(token))
+	except KeyboardInterrupt:
+		loop.run_until_complete(client.close())
+
+	return 0
+
+if __name__ == '__main__':
+	sys.exit(main())
